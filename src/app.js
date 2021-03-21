e232cbb0248fced4992a2be86a91156379b7ba28
magidian <manmandoo123@gmail.com>
2021-03-21 16:13:15 +0800

    final commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburger.addEventListener('click', toggleButton)
